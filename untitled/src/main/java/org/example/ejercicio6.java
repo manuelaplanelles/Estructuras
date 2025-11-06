53f6130b91b0d08f0a6c8f2d3df47afac7e20790
A613\1DAW <manuelaplanelles@gmail.com>
2025-11-06 17:08:54 +0100

    correccion ejercicio6

--- untitled/src/main/java/org/example/ejercicio6.java
+++ untitled/src/main/java/org/example/ejercicio6.java
@@ -4,10 +4,13 @@ public class ejercicio6 {
     static void main() {
         int[] vector = {1, 2, 3, 2, 1};
 
+        boolean es_o_no =true;
+
         for (int i = 0; i < vector.length; i++) {
 
             if (vector[i] == vector[vector.length - 1 - i]) {
                 System.out.println("No es");
+                es_o_no=false;
                 break;
             }
         }
