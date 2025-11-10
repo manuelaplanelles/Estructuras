9d677776155f9ff19da33825e0ca4e0a096e3d60
A613\1DAW <manuelaplanelles@gmail.com>
2025-11-10 20:25:21 +0100

    creamos ejercicio casino y empezamos a ejecutarlo

---
+++ untitled/src/main/java/org/example/Casino.java
@@ -0,0 +1,12 @@
+public class Casino {
+    public static void main(String[] args) {
+        final String colores[] = {"rojo", "negro"};
+        int numeros[] = new int[37];
+
+        for (int i = 0; i < numeros.length; i++) ;
+        numeros[i] = 1;
+    }
+    final String par_impar[] = {"par", "impar"};
+    
+    }
+}
