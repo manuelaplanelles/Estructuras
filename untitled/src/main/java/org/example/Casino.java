c2e59b845a67b0799aff0e3a9ebbbe61dd8528b0
A613\1DAW <manuelaplanelles@gmail.com>
2025-11-10 21:16:59 +0100

    correcciones ejercicio ruleta

--- untitled/src/main/java/org/example/Casino.java
+++ untitled/src/main/java/org/example/Casino.java
@@ -1,45 +1,51 @@
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Casino {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        Random aleatorio = new Random();
 
         final String colores[] = {"rojo", "negro"};
         Integer numeros[] = new Integer[37];
 
-        for (int i = 0; i < numeros.length; i++) ;
+        for (int i = 0; i < numeros.length; i++){
         numeros[i] = i;
-    }
-    final String par_impar[] = {"par", "impar"};
+         }
 
-    System.out.printIn("Introduce tu número (0-36):")
-    int numero_apuesta= teclado.nexline();
+        final String par_impar[] = {"par", "impar"};
 
-    if (Arrays.asList(numeros).contaoms(numero_apuesta)){
-        System.out.printIn ("ERROR. El numero no es valido");
+    System.out.println("Introduce tu número(0-36):");
+    int numero_apuesta = teclado.nextInt();
+
+    if (!Arrays.asList(numeros).contains(numero_apuesta)){
+        System.out.println ("ERROR. El numero no es valido(0-36): ");
         return;
-
-        }
-    if (numero_apuesta!=0){
-        System.out.println("Introduce el color:");
-        String color_apuesta = teclado.next();
-        if (!Arrays.asList(colores).contains(color_apuesta.toLowerCase())) {
-            System.out.println("ERROR: El color debe ser rojo o negro");
-            return;
-        }
-        System.out.println("Introduce par o impar:");
-        String par_impar_apuesta = teclado.next();
-        if (!Arrays.asList(par_impar).contains(par_impar_apuesta.toLowerCase())) {
-            System.out.println("ERROR: La opcion debe ser par o impar");
-            return;
         }
 
-        String color_sorteo = colores[aleatorio.nexInt(2)];
-        int numero_
 
+        String par_impar_apuesta="";
 
+        if (numero_apuesta!=0){
 
+            System.out.println("Introduce color: ");
+            color_apuesta = teclado.next();
+
+            if (!Arrays.asList(colores).contains(color_apuesta.toLowerCase())){
+                System.out.println("ERROR. El color debe ser rojo-negro.");
+                return;
+            }
+
+            System.out.println("Introduce par/impar: ");
+            par_impar_apuesta = teclado.next();
+
+            if (!Arrays.asList(par_impar).contains(par_impar_apuesta.toLowerCase())){
+                System.out.println("ERROR. La opción debe ser par/impar.");
+                return;
+            }
+
+        }
     }
     }
-    }
+
