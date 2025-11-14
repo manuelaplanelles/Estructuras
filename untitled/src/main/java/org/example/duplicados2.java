f84c98c8ee4f6e198139b7e50edfbc2c99b832bc
A613\1DAW <manuelaplanelles@gmail.com>
2025-11-14 16:02:58 +0100

    x

---
+++ untitled/src/main/java/org/example/duplicados2.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class duplicados2 {
+    public static void main(String[] args) {
+        Random aleatorio = new Random();
+        int numeros[] = new int[20];
+
+        for (int i = 0; i< numeros.length; i++){
+            numeros[i]= aleatorio.nextInt(18)+1;
+        }
+        System.out.println(Arrays.toString(numeros));
+
+        Arrays.sort(numeros);
+        System.out.println(Arrays.toString(numeros));
+
+        for (int i=0;i<numeros.length; i++){
+            if (i!=numeros.length-1 && numeros [i]==numeros[i+1]){
+
+                System.out.println("Duplicado: " + numeros[i]);
+                System.out.println(Arrays.toString(numeros));
+                numeros[i] = aleatorio.nextInt(10)+1;
+                Arrays.sort(numeros);
+            }
+        }
+    }
+}
